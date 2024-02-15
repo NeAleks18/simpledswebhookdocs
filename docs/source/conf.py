224c02104262b4d5d234bf3c9ecbb013cf263009
Tochno Ne Aleks <113923625+NeAleks18@users.noreply.github.com>
2024-02-15 20:46:42 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Simple Discord WebHook API'
+copyright = '2024, everyofflineuser'
+author = 'everyofflineuser'
 
 release = '0.1'
 version = '0.1.0'
